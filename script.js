a5b5f831848a4cf2b7e3219417690b9f6689cf72
TAVEINEAE <taveineofficial@gmail.com>
2026-01-07 22:11:23 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
-const productsEl = document.getElementById("products");
+const tg = window.Telegram.WebApp;
+tg.expand();
+
+const container = document.getElementById("products");
 
 fetch("https://taveine.com/wp-json/miniapp/v1/products")
-  .then(res => res.json())
+  .then(r => r.json())
   .then(products => {
-    productsEl.innerHTML = "";
-
-    products.forEach(p => {
+    products.slice(0, 10).forEach(p => {
       const card = document.createElement("div");
       card.className = "product-card";
-
       card.innerHTML = `
-        <img src="${p.image}" alt="${p.name}">
-        <div class="product-info">
-          <h3>${p.name}</h3>
-          <span>${p.price} AED</span>
-        </div>
+        <img src="${p.image}">
+        <h3>${p.name}</h3>
+        <span>${p.price} AED</span>
       `;
-
-      productsEl.appendChild(card);
+      container.appendChild(card);
     });
-  })
-  .catch(err => {
-    productsEl.innerHTML = "Failed to load products";
-    console.error(err);
   });
